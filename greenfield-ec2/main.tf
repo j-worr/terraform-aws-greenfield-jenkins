9d5a676eedff8586ca53698514ed87afdd13200f
Justin Worrell <worrell.justin@gmail.com>
2021-09-22 07:04:51 -0400

    more various changes

--- greenfield-ec2/main.tf
+++ greenfield-ec2/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.16.0"
+      version = "~> 3.5"
     }
   }
 }
@@ -19,9 +19,9 @@ resource "aws_vpc" "myvpc" {
 }
 
 resource "aws_subnet" "mysubnet" {
-  vpc_id =  aws_vpc.myvpc.id
-  cidr_block = "10.0.0.0/24"
-  map_public_ip_on_launch =  true
+  vpc_id                  = aws_vpc.myvpc.id
+  cidr_block              = "10.0.0.0/24"
+  map_public_ip_on_launch = true
 }
 
 
@@ -30,9 +30,33 @@ resource "aws_internet_gateway" "mygateway" {
 }
 
 resource "aws_route" "route" {
-  route_table_id            =  aws_vpc.myvpc.default_route_table_id
-  destination_cidr_block    = "0.0.0.0/0"
-  gateway_id		    = aws_internet_gateway.mygateway.id
+  route_table_id         = aws_vpc.myvpc.default_route_table_id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.mygateway.id
+}
+
+
+resource "aws_security_group" "allow_ssh" {
+  name   = "allow_ssh"
+  vpc_id = aws_vpc.myvpc.id
+
+  ingress {
+    description      = "ssh"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
 }
 
 
@@ -54,11 +78,12 @@ data "aws_ami" "ubuntu" {
 
 
 resource "aws_instance" "myinstance" {
-  ami = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
-  subnet_id = aws_subnet.mysubnet.id  
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.mysubnet.id
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  key_name               = "mykey"
   tags = {
     Name = "example instance"
   }
-
 }
