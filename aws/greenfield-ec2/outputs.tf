a26eaf7aa2d13ba44abf401ba06ed0a343c5602e
Justin Worrell <worrell.justin@gmail.com>
2021-09-25 08:22:03 -0400

    output formatting

--- aws/greenfield-ec2/outputs.tf
+++ aws/greenfield-ec2/outputs.tf
@@ -1,4 +1,4 @@
 output "instance_ip_addr" {
-  value       = aws_instance.myinstance.public_ip
+  value       = "Public IP: ${aws_instance.myinstance.public_ip}"
   description = "The public IP address of the main server instance."
 }
