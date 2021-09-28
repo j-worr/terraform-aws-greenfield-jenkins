00ec94b95862a2707c723db1bebe426ca5bfbb39
j-worr <76519886+j-worr@users.noreply.github.com>
2021-09-28 06:54:48 -0400

    Delete aws/greenfield-ec2 directory

--- aws/greenfield-ec2/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "instance_id" {
-  value       = aws_instance.myinstance.id
-  description = "Instance ID"
-}
-
-output "public_ip_addr" {
-  value       = aws_instance.myinstance.public_ip
-  description = "The public IP address of the main server instance."
-}
